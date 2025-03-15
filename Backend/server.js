f45d73415796c76a508014bcb68a3ac410b3c979
vansh317 <vanshahuja317@gmail.com>
2025-03-15 23:48:19 +0530

    request origin changed

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "hhttps://codequest-frontend-0kz2.onrender.com", 
   credentials: true, 
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: "Content-Type,Authorization",
