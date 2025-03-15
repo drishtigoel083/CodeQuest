a0b3a22cbadaefc23135453de3d6aea743658294
vansh317 <vanshahuja317@gmail.com>
2025-03-16 00:07:57 +0530

    cors headers added

--- Backend/server.js
+++ Backend/server.js
@@ -16,6 +16,13 @@ app.use(cors({
   allowedHeaders: ["Content-Type,Authorization"],
 }));
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://codequest-frontend-0kz2.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
