c4750f8d0bc3192efb4d426b3411cbe751128087
vansh317 <vanshahuja317@gmail.com>
2025-03-18 00:34:05 +0530

    resolved cookies

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -34,6 +34,7 @@ router.post("/login", async (req, res) => {
   const token = jwt.sign({ id: user._id }, process.env.TOKEN, { expiresIn: "1h" });
   res.cookie("jwt", token, {
     httpOnly: true,
+    secure: true,
     sameSite: "none", 
     maxAge: 3600000, 
   });
