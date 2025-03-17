8ce7a6d6e65b8ff79812c9511aac746808e8b152
Sarthak Jain <jainsarthak1811@gmail.com>
2025-03-17 22:06:30 +0530

    cloud database setup

--- Backend/server.js
+++ Backend/server.js
@@ -10,18 +10,18 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: "https://codequest-frontend-0kz2.onrender.com", 
+  origin: "http://localhost:5173", 
   credentials: true, 
   methods: "GET,POST,PUT,DELETE",
   allowedHeaders: ["Content-Type,Authorization"],
 }));
 
-app.options("*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", "https://codequest-frontend-0kz2.onrender.com");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-});
+// app.options("*", (req, res) => {
+//   res.header("Access-Control-Allow-Origin", "https://codequest-frontend-0kz2.onrender.com");
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   res.sendStatus(200);
+// });
 
 app.use(cookieParser());
 app.use(express.json());
